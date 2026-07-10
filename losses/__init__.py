c256ab0ab7a798e1a00e953e08bca242b4ad353d
KosarZarean <kzaran79@gmail.com>
2026-07-10 10:02:10 +0330

    Refactor: Complete losses/__init__.py with proper exports

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,1 +1,24 @@
-# Models package
+"""
+ZESRD-HSV Losses Package
+Loss functions for zero-shot image enhancement training.
+"""
+
+from .perceptual import VGGPerceptualLoss
+from .edge import EdgeLoss
+from .physics import physics_loss
+from .smoothness import smoothness_loss
+from .consistency import consistency_loss
+from .color import color_constancy_loss
+from .gamma import gamma_regularization
+from .total_loss import TotalLoss
+
+__all__ = [
+    'VGGPerceptualLoss',
+    'EdgeLoss',
+    'physics_loss',
+    'smoothness_loss',
+    'consistency_loss',
+    'color_constancy_loss',
+    'gamma_regularization',
+    'TotalLoss',
+]
